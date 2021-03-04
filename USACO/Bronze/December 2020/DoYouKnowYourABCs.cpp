874a0666b0b1176a70893e726683c1b98ff13fec
23yimichael <63271391+23yimichael@users.noreply.github.com>
2021-03-03 22:33:02 -0800

    Create DoYouKnowYourABCs.cpp

---
+++ USACO/Bronze/December 2020/DoYouKnowYourABCs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[7];
+
+int main() {
+    for(int i = 0; i < 7; i++) cin >> a[i];
+    sort(a, a+7);
+    cout << a[0] << " " << a[1] << " " << (a[6]-(a[0]+a[1]));
+}
