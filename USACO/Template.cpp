4c3048426ce3d76bda2931a8ddbef55d19fa5e90
23yimichael <23yimichael@gmail.com>
2021-04-11 00:38:05 -0700

    updated templates

--- USACO/Template.cpp
+++ USACO/Template.cpp
@@ -1,34 +1,5 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-
-#define ll long long
-#define ld long double
-
+#include <bits/stdc++.h>
 using namespace std;
-
-bool debug = false;
-const int mod = 1000000007;
 
 int main() {
   ios::sync_with_stdio(0); cin.tie(0);
